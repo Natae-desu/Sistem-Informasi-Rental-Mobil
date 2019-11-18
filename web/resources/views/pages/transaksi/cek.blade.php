90669157d3c742fc6e200f759efd0c6fdd78053a
Natae-desu <bokuwanata@gmail.com>
2019-11-18 19:47:01 +0700

    Perbaikan sedikit

--- web/resources/views/pages/transaksi/cek.blade.php
+++ web/resources/views/pages/transaksi/cek.blade.php
@@ -1,18 +1,18 @@
 @extends('main')
 
-@section('title','Status Pengiriman')
+@section('title','Transaksi')
 
 @section('content')
 <div class="content-wrapper">
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-6"><h1>Status Pengiriman</h1></div>
+                <div class="col-6"><h1>Transaksi</h1></div>
                 <div class="col-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route("home") }}">Home</a></li>
                        
-                        <li class="breadcrumb-item active">Status Pengiriman</li>
+                        <li class="breadcrumb-item active">Transaksi</li>
                     </ol>
                 </div>
             </div>
@@ -21,7 +21,7 @@
     <div class="content">
         <div class="card">
             <div class="card-header bg-primary text-white">
-                <h3 class="card-title">Status Pengiriman</h3>
+                <h3 class="card-title">Status Pembayaran</h3>
             </div>
             <div class="card-body">
                 <form action=''
@@ -35,45 +35,39 @@
                     method="POST" autocomplete="off">
                     @csrf
                     <div class="form-group">
-                        <label for="kodepemesan">kode pemesanan</label>
+                        <label for="kodepemesan">Kode Pemesanan</label>
                         <input type="text"
                             class="form-control"
                             name="kodepemesan">
                     </div>
                     <div class="form-group">
-                        <label for="nama">nama</label>
+                        <label for="nama">Nama</label>
                         <input type="text"
                             class="form-control"
                             name="nama">
                     </div>
                     
                     <div class="form-group">
-                        <label for="tgltransaksi">tgl transaksi</label>
+                        <label for="tgltransaksi">Tgl transaksi</label>
                         <input type="tgltransaksi"
                             class="form-control"
                             name="tgltransaksi">
                     </div>
                    
                     <div class="form-group">
-                        <label for="statuspembayaran">status pembayran</label>
+                        <label for="statuspembayaran">Bukti Resi</label>
                         <input type="text"
                             class="form-control"
                             name="statuspembayaran">
+                           
                     </div>
                
                     <div class="form-group">
                         <label for="status">Status</label>
                         <select name="status" class="form-control">
-                            <option value="pen">Pending...</option>
-                            <option value="received">RECEIVED</option>
-                            <option value="picked">PICKED UP BY COURIER</option>
-                            <option value="sorting">RECEIVED AT SORTING CENTER</option>
-                            <option value="procsort">PROCESSED AT SORTING CENTER</option>
-                            <option value="departed">DEPARTED FROM TRANSIT</option>
-                            <option value="gateaway">RECEIVED AT ORIGIN GATEAWAY</option>
-                            <option value="warehouse">RECEIVED AT WAREHOUSE</option>
-                            <option value="deliv">WITH DELIVERY COURIER</option>
-                            <option value="done">DELIVERED</option></select>
+                            <option value="pen">Lunas</option>
+                            <option value="pen">Pending</option>
+                            </select>
                     </div>
                     <div class="form-group float-right">
                         
